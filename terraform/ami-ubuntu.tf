e6bba0705393a336dbd3334d966ae5a1489e4e81
Jonathan Wright <jon@than.io>
2018-04-30 18:33:52 +0100

    Add AMI data source for Ubuntu 18.04 LTS image

---
+++ terraform/ami-ubuntu.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Canonical
+  owners = ["099720109477"]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,8 @@ output "bastion_public_ip" {
   description = "Public IP address for the Bastion Server"
   value       = "0.0.0.0"
 }
+
+output "ami_id" {
+  description = "AMI ID of for Ubuntu 16.04 LTS in the current region."
+  value       = "${data.aws_ami.ubuntu.id}"
+}
