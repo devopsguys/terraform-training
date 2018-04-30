fdc5cf3aed2ef7790a4c7f21095cbbf50d6bc03a
Jonathan Wright <jon@than.io>
2018-04-30 19:39:50 +0100

    Add the Web server instances

---
+++ terraform/server-web.tf
@@ -0,0 +1,23 @@
+locals {
+  web_servers_count = "2"
+}
+
+resource "aws_instance" "web" {
+  count         = "${local.web_servers_count}"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
+  key_name      = "${aws_key_pair.ssh.id}"
+
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [
+    "${aws_security_group.web.id}",
+  ]
+
+  tags {
+    Name        = "Training_${var.name}_Web${format("%02d", count.index+1)}"
+    Environment = "${terraform.workspace}"
+    Owner       = "${var.email}"
+  }
+}
